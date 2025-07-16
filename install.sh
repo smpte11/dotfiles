ee4eebb8ed76222fcb1a5b7e76f93e3f2c61aac4
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2025-07-16 11:54:24 +0200

    Try in main install

--- install.sh
+++ install.sh
@@ -1,8 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 # -e: exit on error
 # -u: exit on unset variables
 set -eu
+
+if [[ "$(uname -s)" == "Darwin" ]]; then
+    echo "Running on MacOS"
+    HOMEBREW_PREFIX="/opt/homebrew"
+else
+    echo "Running on Linux"
+    HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew"
+fi
+
+if command -v brew >/dev/null; then
+    echo "brew is already installed"
+else
+    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    (
+        echo
+        echo "eval "$("$HOMEBREW_PREFIX/bin/brew" shellenv)""
+    ) >>"$HOME"/.bashrc
+    eval "$("$HOMEBREW_PREFIX/bin/brew" shellenv)"
+fi
 
 if ! chezmoi="$(command -v chezmoi)"; then
     bin_dir="${HOME}/.local/bin"
