e3c298d8c5e0e990daf679f3573dc61755ff821f
Felix Brousseau-Tremblay <felix.brousseau-tremblay.1@live.com>
2025-07-16 09:20:22 +0200

    Change hook to a before script
     - install devcontainer cli

--- run_once_before_01-install-prerequisites.sh
+++ run_once_before_01-install-prerequisites.sh
@@ -37,15 +37,6 @@ detect_container() {
     return 1
 }
 
-# Call the detection function
-detect_container
-
-if command -v brew >/dev/null; then
-    echo "brew is already installed"
-else
-    NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
 if command -v bw >/dev/null; then
     echo "bitwarden-cli already installed"
 else
